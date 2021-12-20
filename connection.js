ab0d96068d2202f601132a2e66f933985324cb73
benkrakar <benkrakarwalid44@gmail.com>
2021-12-20 21:18:06 +0100

    adding env file

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
-
+import dotenv from 'dotenv';
+dotenv.config();
 let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'todo',
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
 });
 connection.connect((err) => {
   if (err) {
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,19 @@
-const http = require("http");
-const {getTasks} = require("../controller/taskController.js");
-
+const http = require('http');
+const { getTasks } = require('../controller/taskController.js');
+import dotenv from 'dotenv';
+dotenv.config();
 
 const server = http.createServer((req, res) => {
-   switch (req.url) {
-     case "/api/tasks":
-       getTasks(req, res);
-       break;
-     default:
-       res.writeHead(404, { "Content-Type": "application/json" });
-       res.end(JSON.stringify({ message: "route not found ..." }));
-       break;
-   }
+  switch (req.url) {
+    case '/api/tasks':
+      getTasks(req, res);
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'route not found ...' }));
+      break;
+  }
 });
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`run ${PORT}`));
