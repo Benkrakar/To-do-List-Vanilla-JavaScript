d7bcd1d3f45715bbcda30f13ee0eb031476d9cfa
benkrakar <benkrakarwalid44@gmail.com>
2021-12-20 16:38:09 +0100

    create switch cases

--- controller/taskController.js
+++ controller/taskController.js
@@ -9,7 +9,6 @@ async function getTasks(req, res) {
     throw err;
   }
 }
-
 module.exports = {
   getTasks,
 };
--- routes.js
+++ routes.js
@@ -3,13 +3,17 @@ const {getTasks} = require("./controller/taskController.js");
 
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/api/tasks" && req.method === "GET") {
-    getTasks(req, res);
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "route not found ..." }));
-  }
+   switch (req.url) {
+     case "/api/tasks":
+       getTasks(req, res);
+       break;
+     default:
+       res.writeHead(404, { "Content-Type": "application/json" });
+       res.end(JSON.stringify({ message: "route not found ..." }));
+       break;
+   }
 });
+
 
 const PORT = 3000;
 server.listen(PORT, () => console.log(`run ${PORT}`));
